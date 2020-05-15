fa51f0a5e6c7e24e06960586728001ad6f4b04ce
magdamiu <badita.magda@gmail.com>
2020-05-15 16:30:09 +0300

    image analysis: detect average number of purple pixels in the last picture

--- app/src/main/java/com/magdamiu/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/magdamiu/cameraxdemo/MainActivity.kt
@@ -3,9 +3,16 @@ package com.magdamiu.cameraxdemo
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -13,18 +20,26 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
+import java.nio.ByteBuffer
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var previewView: PreviewView
+    private lateinit var helperTextView: TextView
+    private lateinit var executor: Executor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        executor = ContextCompat.getMainExecutor(this)
+
         previewView = findViewById(R.id.preview)
+        helperTextView = findViewById(R.id.helperTextView)
 
         if (areAllPermissionsGranted()) {
             startCamera()
@@ -40,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
             bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(this))
+        }, executor)
     }
 
     private fun bindPreview(cameraProvider: ProcessCameraProvider) {
@@ -51,9 +66,25 @@ class MainActivity : AppCompatActivity() {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
 
-        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        var camera = cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            setupImageAnalysis(),
+            preview
+        )
 
         preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
+    }
+
+    private fun setupImageAnalysis(): ImageAnalysis {
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        imageAnalysis.setAnalyzer(executor, PurpleColorAnalyser())
+
+        return imageAnalysis;
     }
 
     override fun onRequestPermissionsResult(
@@ -79,5 +110,32 @@ class MainActivity : AppCompatActivity() {
         private const val CAMERA_REQUEST_PERMISSION_CODE = 10
         private val PERMISSIONS =
             arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    inner class PurpleColorAnalyser() : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()
+            val data = ByteArray(remaining())
+            get(data)
+            return data
+        }
+
+        override fun analyze(image: ImageProxy) {
+            val currentTimestamp = System.currentTimeMillis()
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)
+            ) {
+                val buffer = image.planes[0].buffer
+                val data = buffer.toByteArray()
+                val pixels = data.map { it.toInt() and 0x9370DB }
+                var averagePurplePixels = pixels.average()
+
+                helperTextView.text = "Average purple pixels: $averagePurplePixels"
+                //Log.e("PURPLE", "Average purple pixels: $averagePurplePixels")
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
     }
 }
